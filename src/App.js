1bba28ec2d1e135e1144647b221c98c65772c16f
Adam Jones <metacosmgame@gmail.com>
2024-12-26 15:04:10 -0500

    yo removed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Animation from './Animation/Animation';
 import Design from './Design/Design';
 import Storyboarding from './Storyboarding/Storyboarding';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//yo
 
 function App() {
   return (
