571d52cee8b23e4d502cd6ee8ee5d0e257d35b26
ajjonesx3 <ajjonesx3@gmail.com>
2025-02-17 18:07:31 -0500

    Added wills actually art (still requires storyboards)

--- src/Animation/Animation.js
+++ src/Animation/Animation.js
@@ -8,9 +8,13 @@ import {useState} from 'react';
 
 const columnText = "This Page showcases my animation. ranging from simple quick films to big spectacles. my style focuses on energy and the feel of the scene. I accomplish this through my dynamic and creative angles as well as background animation and complex effects animation.";
 
+const videos = require.context('../WillData/Art/Animation', false);
+const videoList = videos.keys().map(video=> videos(video));
+console.log(videoList);
+
 const Animation = () => {
 
-    const [selectedImage, setSelectedImage] = useState(img1);
+    const [selectedImage, setSelectedImage] = useState(videoList[0]);
 
     const handleClick = (imageToSelect) => {
         setSelectedImage(imageToSelect);
@@ -21,12 +25,15 @@ const Animation = () => {
             <LeftColumn text={columnText}/>
             <div className="rightSide">
                 <div className="selectedImage">
-                    <img src={selectedImage} alt='image1'/>
+                    <video controls src={selectedImage} alt='selected video'/>
                 </div>
                 <div className="otherImages">
                     <img onClick={handleClick.bind(this, img3)} src={img3} alt='image3'/>
                     <img onClick={handleClick.bind(this, img1)} src={img1} alt='image1'/>
                     <img onClick={handleClick.bind(this, img2)} src={img2} alt='image2'/>
+                    {videoList.map((video,index)=>{
+                        return <video onClick={handleClick.bind(this,video)} key={index} src={video} alt={`image-${index}`} />
+                    })}
                 </div>
             </div>
         </div>
--- src/Design/Design.js
+++ src/Design/Design.js
@@ -1,51 +1,26 @@
 import "./Design.css";
 import LeftColumn from '../LeftColumn/LeftColumn';
 
-import {useEffect,useState} from 'react';
-
-import img1 from './img1.jpg';
-import img2 from './img2.jpg';
-import img3 from './img3.jpg';
-import img4 from './img4.jpg';
+//import {useEffect,useState} from 'react';
 
 const columnText = "This Page features my designs, ranging from promotional pieces, posters, to character designs. I mainly work digitally using a rougher texture to add a sketchy feel to my pieces with subtle color. I want to focus more on graphic design and using my skills to promote brands as well as design characters to be used in animation.";
 
+const images = require.context('../WillData/Art/Design', false);
+const imageList = images.keys().map(image=> images(image));
 
-const i1 = (<img src={img1} alt="img1"/>);
-const i2 = (<img src={img2} alt="img2"/>);
-const i3 = (<img src={img3} alt="img3"/>);
-const i4 = (<img src={img4} alt="img4"/>);
+console.log(imageList);
 
-const imagesArray = [i1,i2,i3,i4];
-
-const randomSelection = arr => {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-}
-
-const makeRandomImages = n => {
-    let ret = [];
-    for(let i=0;i<n;i++){
-        const newImage = randomSelection(imagesArray);
-        ret.push(randomSelection(imagesArray));
-    }
-    return ret;
-}
 
 const Design = () => {
-
-    const [collageImages,setCollageImages] = useState([]);
-    useEffect(()=>{
-        const newImages = makeRandomImages(25);
-        setCollageImages(newImages);
-    },[]);
 
     return (
         <div className="design">
             <LeftColumn text={columnText}/>
             <div className="rightSide">
                 <div className="collage">
-                    {collageImages}
+                    {imageList.map((image,index)=>{
+                        return <img src={image} key={index} alt={`image-${index}`}/>
+                    })}
                 </div>          
             </div>
         </div>
